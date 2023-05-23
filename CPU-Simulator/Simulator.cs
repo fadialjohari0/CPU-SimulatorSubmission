11c318dcd0b2a5ce2d546956c9ef107c2f92d541
fadialjohari0 <fadialjohari0@sociumtech.com>
2023-05-24 00:04:02 +0300

    Added Constructer & Added StartSimulation Method

--- CPU-Simulator/Simulator.cs
+++ CPU-Simulator/Simulator.cs
@@ -2,10 +2,44 @@ namespace CPU
 {
     public class Simulator
     {
-        public void StartSimulation(List<Task> tasks, List<Processor> processors, ref int clockCycle)
+        private List<Task> tasks;
+        private List<Processor> processors;
+        private int clockCycle;
+
+        public PriorityQueue<Task, int> HighPriorityQueue { get; set; } = new PriorityQueue<Task, int>();
+        public PriorityQueue<Task, int> LowPriorityQueue { get; set; } = new PriorityQueue<Task, int>();
+        public PriorityQueue<Task, int> LowPriorityWaitingQueue { get; set; } = new PriorityQueue<Task, int>();
+
+
+        public Simulator(List<Task> tasks, List<Processor> processors, int clockCycle)
+        {
+            this.tasks = tasks;
+            this.processors = processors;
+            this.clockCycle = clockCycle;
+        }
+
+        public void StartSimulation()
         {
             IScheduler scheduler = new Scheduler();
-            scheduler.Schedule(tasks!, processors, ref clockCycle);
+
+            while (tasks.Any(task => task.State != TaskState.COMPLETED))
+            {
+                clockCycle++;
+
+                bool allProcessorsBusy = processors.All(processor => processor.State == ProcessorState.BUSY);
+                bool anyProcessorHasLowPriorityTask = processors.Any(processor => processor.CurrentTask?.Priority == "Low");
+
+                if (allProcessorsBusy && HighPriorityQueue.Count > 0 && anyProcessorHasLowPriorityTask)
+                {
+                    scheduler.InterruptLowTasks(processors, clockCycle, LowPriorityWaitingQueue, HighPriorityQueue);
+                }
+                else
+                {
+                    scheduler.AssignTasksToProcessors(processors, clockCycle, LowPriorityWaitingQueue, HighPriorityQueue, LowPriorityQueue);
+
+                    scheduler.CreateTasks(tasks, clockCycle, HighPriorityQueue, LowPriorityQueue);
+                }
+            }
         }
     }
 }
