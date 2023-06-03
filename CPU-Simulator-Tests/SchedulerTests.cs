2cf77a4f2f708c6514c2f23d6823827d476b9be9
fadialjohari0 <fadialjohari0@sociumtech.com>
2023-06-03 23:11:42 +0300

    Added Scheduler Testings

--- CPU-Simulator-Tests/SchedulerTests.cs
+++ CPU-Simulator-Tests/SchedulerTests.cs
@@ -1,38 +1,81 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CPU;
+using System.Collections.Generic;
 
-namespace CPU;
-
-[TestClass]
-public class SchedulerTests
+namespace CPUTest
 {
-    [TestMethod]
-    public void InitializeProcessors_NumberOfProcessors_CreatesCorrectAmountOfProcessors()
+    [TestClass]
+    public class SchedulerTests
     {
-        // Arrange
-        ProcessorManager processorManager = new ProcessorManager();
-        int numOfProcessors = 5;
-
-        // Act
-        var result = processorManager.InitializeProcessors(numOfProcessors);
-
-        // Assert
-        Assert.AreEqual(numOfProcessors, result.Count);
-    }
-
-    [TestMethod]
-    public void InitializeProcessors_NumberOfProcessors_CreatesCorrectProcessorsId()
-    {
-        // Arrange
-        ProcessorManager processorManager = new ProcessorManager();
-        int numOfProcessors = 5;
-
-        // Act
-        var result = processorManager.InitializeProcessors(numOfProcessors);
-
-        // Assert
-        for (int i = 0; i < 5; i++)
+        [TestMethod]
+        public void TestAssignTasksToProcessors_HighPriorityQueue()
         {
-            Assert.AreEqual($"P{i + 1}", result[i].Id);
+            // Arrange
+            IScheduler scheduler = new Scheduler();
+            PriorityQueue<Task, int> highPriorityQueue = new PriorityQueue<Task, int>();
+            PriorityQueue<Task, int> lowPriorityQueue = new PriorityQueue<Task, int>();
+            PriorityQueue<Task, int> lowPriorityWaitingQueue = new PriorityQueue<Task, int>();
+            List<Processor> processors = new List<Processor>();
+
+            Task highPriorityTask = new Task
+            {
+                Priority = "High",
+                RequestedTime = 5,
+                CreationTime = 0,
+            };
+            highPriorityQueue.Enqueue(highPriorityTask, highPriorityTask.RequestedTime);
+
+            Processor idleProcessor = new Processor
+            {
+                State = ProcessorState.IDLE
+            };
+            processors.Add(idleProcessor);
+
+            // Act
+            scheduler.AssignTasksToProcessors(processors, 0, lowPriorityWaitingQueue, highPriorityQueue, lowPriorityQueue);
+
+            // Assert
+            Assert.AreEqual(TaskState.EXECUTING, highPriorityTask.State);
+            Assert.AreEqual(ProcessorState.BUSY, idleProcessor.State);
+            Assert.AreEqual(highPriorityTask, idleProcessor.CurrentTask);
+            Assert.AreEqual(0, highPriorityQueue.Count);
+        }
+
+        [TestMethod]
+        public void CreateTasks_NewTasks_AddsThemToCorrectQueues()
+        {
+            // Arrange
+            IScheduler scheduler = new Scheduler();
+            PriorityQueue<Task, int> highPriorityQueue = new PriorityQueue<Task, int>();
+            PriorityQueue<Task, int> lowPriorityQueue = new PriorityQueue<Task, int>();
+            List<Task> tasks = new List<Task>();
+
+            Task highPriorityTask = new Task
+            {
+                Priority = "High",
+                RequestedTime = 5,
+                CreationTime = 0,
+            };
+
+            Task lowPriorityTask = new Task
+            {
+                Priority = "Low",
+                RequestedTime = 3,
+                CreationTime = 0,
+            };
+
+            tasks.Add(highPriorityTask);
+            tasks.Add(lowPriorityTask);
+
+            // Act
+            scheduler.CreateTasks(tasks, 0, highPriorityQueue, lowPriorityQueue);
+
+            // Assert
+            Assert.AreEqual(1, highPriorityQueue.Count);
+            Assert.AreEqual(highPriorityTask, highPriorityQueue.Peek());
+
+            Assert.AreEqual(1, lowPriorityQueue.Count);
+            Assert.AreEqual(lowPriorityTask, lowPriorityQueue.Peek());
         }
     }
 }
